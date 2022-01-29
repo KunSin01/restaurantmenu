a711fc8c2657d75fab517372ef55945e9eecb799
Kunal Singh <singhkunal579@gmail.com>
2022-01-29 22:46:23 +0530

    first commit

---
+++ src/MenuAPI.js
@@ -0,0 +1,77 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/pancake.jpg",
+      name: "Pancake",
+      category: "breakfast",
+      price: "120₹",
+      description: "I love Pancake"   
+    },
+  
+    {
+      id: 2,
+      image: "images/coffee.jpg",
+      name: "Coffee",
+      category: "breakfast",
+      price: "90₹",
+      description: "I love Coffee"    
+    },
+    {
+      id: 3,
+      image: "images/waffle.jpg",
+      name: "Waffle",
+      category: "breakfast",
+      price: "100₹",
+      description: "I love Waffle"
+    },
+    {
+      id: 4,
+      image: "images/pizza.jpg",
+      name: "Pizza",
+      category: "lunch",
+      price: "150₹",
+      description: "I love Pizza"
+    },
+    {
+      id: 5,
+      image: "images/dosa.jpg",
+      name: "Dosa",
+      category: "lunch",
+      price: "180₹",
+      description: "I love Dosa"
+    },
+    {
+      id: 6,
+      image: "images/momo.jpg",
+      name: "Momo",
+      category: "lunch",
+      price: "80₹",
+      description: "I love Momo"
+    },
+    {
+      id: 7,
+      image: "images/chicken.jpg",
+      name: "Butter Chicken",
+      category: "dinner",
+      price: "160₹",
+      description: "I love Butter Chicken"
+    },
+    {
+      id: 8,
+      image: "images/burrito.jpg",
+      name: "Burrito",
+      category: "dinner",
+      price: "60₹",
+      description: "I love Burrito"
+    },
+    {
+      id: 9,
+      image: "images/biryani.jpg",
+      name: "Biryani",
+      category: "dinner",
+      price: "100₹",
+      description: "I love Biryani"
+    },
+  ];
+  
+  export default Menu;
